50560d3ea39cefb26178dc86f059f340161c213d
vasich122 <vasilij.sovarenko@icloud.com>
2025-09-23 14:42:29 +0300

    Выполнение задания в рамках 2 практической работы

--- lib/main.dart
+++ lib/main.dart
@@ -9,41 +9,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ColorChangerPage(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class ColorChangerPage extends StatefulWidget {
-  const ColorChangerPage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
   @override
-  State<ColorChangerPage> createState() => _ColorChangerPageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _ColorChangerPageState extends State<ColorChangerPage> {
-  Color _backgroundColor = Colors.white;
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  void _changeColor() {
+  void _incrementCounter() {
     setState(() {
-      _backgroundColor =
-      _backgroundColor == Colors.white ? Colors.lightBlue : Colors.white;
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: _backgroundColor,
       appBar: AppBar(
-        title: const Text("Смена цвета"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: _changeColor,
-          child: const Text("Изменить фон"),
+      body: const Center(
+        child: Text(
+          'Соваренко Василий Васильевич\nГруппа: ИКБО-06-22\n22И1798',
+          textAlign: TextAlign.center,
         ),
       ),
     );

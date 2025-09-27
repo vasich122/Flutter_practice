cc922e8a3f0c4109dd2bbdd9c28565c4b06b739b
vasich122 <vasilij.sovarenko@icloud.com>
2025-09-27 21:45:08 +0300

    Смена Widget-а Column на Row

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(

cf35aefed295e5495864fd2410ef20637e6fb861
vasich122 <vasilij.sovarenko@icloud.com>
2025-10-07 21:27:39 +0300

    Создание и реализация виджета GradeTable

---
+++ lib/features/grade/widgets/grade_table.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../models/grade_model.dart';
+import 'grade_row.dart';
+
+class GradeTable extends StatelessWidget {
+  final List<GradeModel> grades;
+  final ValueChanged<String> onRemove;
+
+  const GradeTable({
+    super.key,
+    required this.grades,
+    required this.onRemove,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: grades.length,
+      itemBuilder: (context, index) {
+        return GradeRow(
+          grade: grades[index],
+          onRemove: onRemove,
+        );
+      },
+    );
+  }
+}
